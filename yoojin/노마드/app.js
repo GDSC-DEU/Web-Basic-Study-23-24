1a1f6b69aa2e6957a2eb694a242187b6d0e727d3
yoojin <76746507+syj318@users.noreply.github.com>
2024-08-20 21:40:40 +0900

    노마드(4.1~4.5)

--- yoojin/노마드/app.js
+++ yoojin/노마드/app.js
@@ -1,12 +1,16 @@
+const loginForm = document.querySelector("#login-form ");
 const loginInput = document.querySelector("#login-form input");
-const loginButton = document.querySelector("#login-form button");
+const greeting = document.querySelector("#greeting");
 
-function onLoginBtnClick() {
-  console.log("hello", loginInput.value);
+const HIDDEN_CLASSNAME = "hidden";
+
+function onLoginSubmit(event) {
+  event.preventDefault();
+  loginForm.classList.add(HIDDEN_CLASSNAME);
+  const username = loginInput.value;
+  localStorage.setItem("username", username);
+  greeting.innerText = "Hello" + username;
+  greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
-if (loginButton) {
-  loginButton.addEventListener("click", onLoginBtnClick);
-} else {
-  console.error("loginButton이 존재하지 않습니다.");
-}
+loginForm.addEventListener("submit", onLoginSubmit);
