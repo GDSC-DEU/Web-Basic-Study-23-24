0b3f5694ca70044afa973fb49b88dbfb2d6b3b5e
yoojin <76746507+syj318@users.noreply.github.com>
2024-08-22 18:52:38 +0900

    노마드(4.6~5.2)

---
+++ yoojin/노마드/js/greetings.js
@@ -0,0 +1,28 @@
+const loginForm = document.querySelector("#login-form ");
+const loginInput = document.querySelector("#login-form input");
+const greeting = document.querySelector("#greeting");
+
+const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
+
+function onLoginSubmit(event) {
+  event.preventDefault();
+  loginForm.classList.add(HIDDEN_CLASSNAME);
+  const username = loginInput.value;
+  localStorage.setItem("USERNAME_KEY", username);
+  paintingGreetings(username);
+}
+
+function paintingGreetings(username) {
+  const username = localStorage.getItem(USERNAME_KEY);
+  greeting.innerText = "Hello" + username;
+  greeting.classList.remove(HIDDEN_CLASSNAME);
+}
+const savedUsername = localStorage.getItem("USERNAME_KEY");
+
+if (savedUsername === null) {
+  loginForm.classList.remove(HIDDEN_CLASSNAME);
+  loginForm.addEventListener("submit", onLoginSubmit);
+} else {
+  paintingGreetings(savedUsername);
+}
