2c28176da7951470cf08b3cc5ed4066c687d7ab4
MinGyeong Kim <mink0502k@gmail.com>
2024-08-30 00:18:11 +0900

    1

--- minkyong/JavaScript Study/js/app.js
+++ minkyong/JavaScript Study/js/app.js
@@ -2,7 +2,7 @@ const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
 const greeting = document.querySelector("#greeting");
 
-const HIDDEN_CLASSNAME = "hidden";
+
 
 function onLoginSubmit(event){
     event.preventDefalut();
